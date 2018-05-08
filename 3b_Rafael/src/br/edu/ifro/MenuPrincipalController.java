8d31dc19dd4daf5128cec0dc47df1b37222824c0
Rafael <Rafael@LAPTOP-HD5E8R15>
2018-05-08 00:21:06 -0300

    Gravar no banco de dados a partir da tela de cadastro

--- 3b_Rafael/src/br/edu/ifro/MenuPrincipalController.java
+++ 3b_Rafael/src/br/edu/ifro/MenuPrincipalController.java
@@ -54,7 +54,7 @@ public class MenuPrincipalController implements Initializable {
         try{
             FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(),900,682);
+            Scene scene = new Scene(fxmlLoader.load(),400,400);
             Stage stage = new Stage();
             stage.setTitle("Cadastrar Alunos");
             stage.setScene(scene);
